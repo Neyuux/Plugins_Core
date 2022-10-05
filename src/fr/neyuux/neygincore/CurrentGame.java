4848f2bdcfb1d742d74e4c719bf5452ed50a61b1
Neyuux <60991177+Neyuux@users.noreply.github.com>
2022-10-05 23:46:55 +0200

    update locations 2

--- src/fr/neyuux/neygincore/CurrentGame.java
+++ src/fr/neyuux/neygincore/CurrentGame.java
@@ -1,12 +1,13 @@
 package fr.neyuux.neygincore;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 
 public enum CurrentGame {
 
-	NONE("None", Core.prefix, null, null, "Neyuux_", "Neyuux_", 0, 0, "Core", -565, 23.2, 850),
+	NONE("None", Core.prefix, null, null, "Neyuux_", "Neyuux_", 0, 0, "Core", -6.5, 49, -5.5),
 	PVPKITS("PvPKits", "§e[§4§lP§b§lv§4§lP §9§lKits§r§e]", Material.MUSHROOM_SOUP, "§7Choisis ton kit avec stratégie en compagnie de tes teammates puis va combattre les autres équipes !", "Neyuux_", "Neyuux_", 2, Integer.MAX_VALUE, "PvPKits", -6.096, 5.1, -2.486),
-	LG("LG", "§c§lLoups§e§l-§6§lGarous", Material.MONSTER_EGG, "§7Mode de jeu stratégique, obtient ton rôle, joue et gagne avec ton camp. Sauras-tu rentrer dans la tête de tes amis ?", "Neyuux_", "Philippe des Pallières et Hervé Marly", 5, 26, "LG", 494, 12.2, 307),
+	LG("LG", "§c§lLoups§e§l-§6§lGarous", Material.MONSTER_EGG, "§7Mode de jeu stratégique, obtient ton rôle, joue et gagne avec ton camp. Sauras-tu rentrer dans la tête de tes amis ?", "Neyuux_", "Philippe des Pallières et Hervé Marly", 5, 26, "LG", 130, 16.5, 380),
 	TOURNOI("Tournoi", "§6§lTournoi", Material.GOLD_HELMET, "§7Tournoi en 1 contre 1 avec comme thème la coupe du monde.", "Neyuux_", "Neyuux_", 2, 64, "Tournoi", -579, 84, 336),
 	UHC("UHC", "§e§lUHC", Material.GOLDEN_APPLE, "§7C'est un UHC tu connais.", "Neyuux_", "Aucun", 2, Integer.MAX_VALUE);
 
